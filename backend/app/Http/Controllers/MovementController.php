31de2353b361e918bb767858d8e6e99614b8c80d
Kevin Nelson <knelson@ramphealth.com>
2024-07-06 15:37:20 -0500

    Added API

---
+++ backend/app/Http/Controllers/MovementController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MovementController extends Controller
+{
+    public function index() {
+        return "test";
+    }
+}
