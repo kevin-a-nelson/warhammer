ada381065f6035837762fb568c033a2d7f8e8ba3
Kevin Nelson <knelson@ramphealth.com>
2024-07-06 15:50:29 -0500

    Added mysql db

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovementController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Add route to movement controller index
-Route::get('/movement', 'MovementController@index');
+Route::get('/movements', [MovementController::class, 'index']);
