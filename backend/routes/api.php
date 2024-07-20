cf5e7503ccb276795ebc9135bddc9d9701585959
kevin-a-nelson <nelsonkevin767@gmail.com>
2024-07-20 04:38:09 -0500

    Added minatures controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovementController;
+use App\Http\Controllers\MinatureController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/movements', [MovementController::class, 'index']);
+Route::get('/minatures', [MinatureController::class, 'index']);
