ede1aee13246835bec00ad535ae9f24f648f330d
kevin-a-nelson <nelsonkevin767@gmail.com>
2024-07-21 19:30:13 -0500

    Removed JWT

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 
 
-Route::post('/login', [LoginController::class, 'login']);
-Route::get('/protected-endpoint', [LoginController::class, 'protectedEndpoint']);
 Route::post('/users', [UserController::class, 'create']);
